e1d9441022b98bd7c8507708f8d1847bef187a80
Diego Casermeiro Gálvez <122561493+diegokser@users.noreply.github.com>
2023-05-07 18:20:28 +0000

    cambios en archivo app.js

--- app.js
+++ app.js
@@ -10,13 +10,11 @@ const fromEuroToDollar = (euro) => {
 console.log (fromEuroToDollar(1))
 
 const fromDollarToYen = (dollar) => {
-    //return dollar * (oneEuroIs["USD"] * oneEuroIs["JPY"])
     return dollar * (oneEuroIs["JPY"] / oneEuroIs["USD"]);
 }
 console.log (fromDollarToYen(1))
 
 const fromYenToPound = (yen) => {
-    //return yen * (oneEuroIs["JPY"] * oneEuroIs["GBP"])
     return yen * oneEuroIs["GBP"] / oneEuroIs["JPY"];
 }
 console.log (fromYenToPound(1))
